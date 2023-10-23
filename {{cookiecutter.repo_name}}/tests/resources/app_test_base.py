1e6362333c5a1d6ec1567409a304faac5492db47
anyesh <sthacruz@gmail.com>
2023-10-23 12:00:13 +1100

    Update template

--- {{cookiecutter.repo_name}}/tests/resources/app_test_base.py
+++ {{cookiecutter.repo_name}}/tests/resources/app_test_base.py
@@ -1,47 +1,55 @@
 from pathlib import Path
 
 from flask_testing import TestCase
-from testcontainers.postgres import PostgresContainer
-
 from src import api
 from src.config import Config
 from src.create_app import create_app
 from src.domain import SQLALCHEMY_DATABASE_URI
-
-from src.infrastructure import sqlalchemy_db as db, setup_sqlalchemy
-
+# from src.infrastructure import setup_sqlalchemy
+from src.infrastructure import sqlalchemy_db as db
+from testcontainers.postgres import PostgresContainer
 
 PROJECT_ROOT = str(Path(__file__).parent.parent.parent)
 
 
-class AppTestBase(TestCase):
+class SqliteDBContainer:
+    def get_connection_url(self):
+        return "sqlite:///:memory:"
 
+    def stop(self):
+        pass
+
+
+class AppTestBase(TestCase):
     def setup_database(self):
-        self.postgres_container = PostgresContainer(image="postgres:14")
-        self.postgres_container.start()
-        self.app.config[SQLALCHEMY_DATABASE_URI] = \
-            self.postgres_container.get_connection_url()
-        setup_sqlalchemy(self.app)
+        self.db_container = self._setup_postgres()
+        self.app.config[
+            SQLALCHEMY_DATABASE_URI
+        ] = self.db_container.get_connection_url()
+        # setup_sqlalchemy(self.app)
         self.clear_database()
 
-    def teardown_database(self):
+    def _setup_postgres(self):
+        try:
+            return PostgresContainer(image="postgres:14").start()
+        except Exception as e:
+            return SqliteDBContainer()
 
-        if self.postgres_container is not None:
+    def teardown_database(self):
+        if self.db_container is not None:
             db.session.commit()
             db.session.close()
-            self.postgres_container.stop()
+            self.db_container.stop()
 
     def clear_database(self):
-
-        if self.postgres_container is not None:
+        if self.db_container is not None:
             db.drop_all()
             db.create_all()
 
     def create_app(self):
         config = Config()
         config["TESTING"] = True
-        app = create_app(config, dependency_container_packages=[api])
-        return app
+        return create_app(config, dependency_container_packages=[api])
 
     def tearDown(self) -> None:
         self.teardown_database()
