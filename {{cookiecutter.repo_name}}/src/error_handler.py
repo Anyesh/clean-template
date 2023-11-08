136e0f1d2cb25907d1b2c73467bd04f897277475
anyesh <sthacruz@gmail.com>
2023-11-08 15:57:37 +1100

    Update error handlers

--- {{cookiecutter.repo_name}}/src/error_handler.py
+++ {{cookiecutter.repo_name}}/src/error_handler.py
@@ -3,9 +3,10 @@ from typing import Dict, List
 
 import marshmallow.exceptions as marshmallow_exceptions
 from flask import jsonify
+from sqlalchemy.exc import NoResultFound
 from werkzeug.exceptions import HTTPException
 
-from src.infrastructure import ClientException, ApiException
+from src.infrastructure import ApiException, ClientException
 
 logger = logging.getLogger(__name__)
 
@@ -16,10 +17,9 @@ def setup_error_handler(app):
     """
 
     def create_error_response(error_message, status_code: int = 400):
-
         # Remove the default 404 not found message if it exists
         if not isinstance(error_message, Dict):
-            error_message = error_message.replace("404 Not Found: ", '')
+            error_message = error_message.replace("404 Not Found: ", "")
 
         response = jsonify({"error_message": error_message})
         response.status_code = status_code
@@ -29,10 +29,8 @@ def setup_error_handler(app):
         errors_message = {}
 
         for key in errors:
-
             if isinstance(errors[key], Dict):
-                errors_message[key] = \
-                    format_marshmallow_validation_error(errors[key])
+                errors_message[key] = format_marshmallow_validation_error(errors[key])
 
             if isinstance(errors[key], List):
                 errors_message[key] = errors[key][0].lower()
@@ -49,15 +47,16 @@ def setup_error_handler(app):
         elif isinstance(error, ClientException):
             return create_error_response(
                 "Currently a dependent service is not available, "
-                "please try again later", 503
+                "please try again later",
+                503,
             )
         elif isinstance(error, ApiException):
-            return create_error_response(
-                error.error_message, error.status_code
-            )
+            return create_error_response(error.error_message, error.status_code)
         elif isinstance(error, marshmallow_exceptions.ValidationError):
             error_message = format_marshmallow_validation_error(error.messages)
             return create_error_response(error_message)
+        elif isinstance(error, NoResultFound):
+            return create_error_response("The requested resource was not found", 404)
         else:
             # Internal error happened that was unknown
             return "Internal server error", 500
